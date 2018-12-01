387b279418b10123f1b94cbdb56badd095807a6f
Erika Takahara <erika.takahara@gmail.com>
2018-11-30 23:43:12 -0200

    [gh-pages2] Testing adding doc as outputDir

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
-module.exports = require("marko-starter").projectConfig({
-  name: "marko-starter-demo" // Optional, but added here for demo purposes
+module.exports = require('marko-starter').projectConfig({
+  name: 'marko-starter-demo',
+  outputDir: 'doc',
 });
